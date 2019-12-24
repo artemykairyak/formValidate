af97b8f12662a3424e297ec53dfac9d595cd62fa
artemykairyak <artemykairyak@yandex.com>
2019-12-24 17:15:54 +0400

    24.12.19

--- build/vendors/js/validateForm.js
+++ build/vendors/js/validateForm.js
@@ -1,16 +1,20 @@
+function formValidate(options) {
+    let vf = new validateForm(options)
+}
+
 class validateForm {
     constructor(options) {
         this.form = options.form;
         this.length = options.form.children.length;
         this.errors = [];
-        this.emailRexExp = options.emailRegExp || /^[-._a-z0-9]+@(?:[a-z0-9][-a-z0-9]+\.)+[a-z]{2,6}$/;
+        this.emailRegExp = options.emailRegExp || /^[-._a-z0-9]+@(?:[a-z0-9][-a-z0-9]+\.)+[a-z]{2,6}$/;
         this.phoneRegExp = options.phoneRegExp || /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/;
         this.url = options.url;
         this.data = {};
         this.onLoadStart = options.onLoadStart;
         this.onSuccess = options.onSuccess;
         this.onError = options.onError;
-        this.success = false;
+
         this.validation();
     }
 
@@ -19,7 +23,7 @@ class validateForm {
 
         let inputs = [];
         for (let i = 0; i < this.length; i++) {
-            if (form.children[i].type && form.children[i].type !== 'submit') {
+            if (form.children[i].type && form.children[i].type !== 'button' && form.children[i].type !== 'submit') {
                 inputs.push(form.children[i]);
             }
         }
@@ -29,40 +33,26 @@ class validateForm {
 
             if ((required && inputs[i].value === '') ||
                 (required && inputs[i].getAttribute('data-role').includes('checkbox') && !inputs[i].checked)) {
-                console.log('if');
                 this.errors.push(inputs[i]);
-                console.log(inputs[i].value);
             } else if (inputs[i].getAttribute('data-role').includes('phone')) {
-                console.log('phone')
                 this.phoneValidation(inputs[i])
             } else if (inputs[i].getAttribute('data-role').includes('email')) {
-                console.log('email')
                 this.emailValidation(inputs[i])
             } else {
                 let key = inputs[i].getAttribute('name');
-                console.log(key)
                 this.data[key] = inputs[i].value;
-                console.log(this.data)
-                //this.errors.pop(inputs[i]);
                 inputs[i].classList.remove('error');
             }
         }
 
         if (this.errors.length) {
             this.errors.forEach(item => {
-                console.log(item)
-
                 item.classList.add('error');
                 return false;
             });
         } else {
-        	this.success = true;
             this.sendInfo(this.data, this.url);
-            console.log('success')
         }
-
-
-        return true;
     }
 
     phoneValidation = input => {
@@ -92,37 +82,26 @@ class validateForm {
     }
 
     sendInfo = (data, url) => {
-        console.log(data)
         let request = new XMLHttpRequest();
-        console.log(request)
         request.open('POST', url, true);
         request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
 
-        request.onloadstart = function() {
-        	console.log('load start')
+        request.onloadstart = () => {
+            this.onLoadStart();
         }
 
-        request.onload = function() {
-            if (this.status >= 200 && this.status < 400) {
-                // Success!
+        request.onload = () => {
+            if (request.status >= 200 && request.status < 400) {
                 this.onSuccess();
-
-           
             } else {
-                // We reached our target server, but it returned an error
-
+                this.onError();
             }
         };
 
-        request.onerror = this.onError();
+        request.onerror = () => {
+            this.onError();
+        };
 
-    
-
-        request.send(data);
+        request.send(JSON.stringify(data));
     }
-
-    debug() {
-        console.log(this.form);
-    }
-
 }
--- src/js/script.js
+++ src/js/script.js
@@ -1,29 +1,21 @@
-var form = document.querySelector('.form');
+let form = document.querySelector('.form');
 
 form.addEventListener('submit', function(e) {
-	e.preventDefault()
-    let a = new validateForm({
+    e.preventDefault();
+    formValidate({
         form: form,
         phoneRegExp: null,
-        emailRexExp: null,
+        emailRegExp: null,
         url: 'http://cors-anywhere.herokuapp.com/http://shop.bitrix24.asap-lp.ru/form_test/index.php',
-        onSuccess: (e) => {
-        	console.log(e);
-        	e.preventDefault();
-        } ,
-        onError: (e) => {
-        	console.log('eeeeeeeeeee')
-        	console.log(e);
-        	e.preventDefault();
+        onLoadStart: () => {
+            console.log('load start');
+        },
+        onSuccess: () => {
+            console.log('success');
+            alert(1)
+        },
+        onError: () => {
+            console.log('error')
         }
     });
-
-    
-    	
-    
-
-    console.log(a.success)
-
-   
-
 })
